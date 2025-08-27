3e690a9903de0c32440c091f77e8f0b92182f9ef
KavyaPratap Singh <kavyapratapsingh221@gmail.com>
2025-08-27 09:30:14 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,23 @@
 const nextConfig = {
     images: {
         remotePatterns: [
-            {
+            /** @type {import('next').NextConfig} */
+
+              experimental: {
+                serverActions: {
+                  bodySizeLimit: '10mb',
+                },
+              },
+              images: {
+                remotePatterns: [
+                  {
+                    protocol: 'https',
+                    hostname: 'uzippzfxkhteeajhcppp.supabase.co',
+                    port: '',
+                    pathname: '/storage/v1/object/public/**',
+                  },
+            
+            {    
                 protocol: 'https',
                 hostname: 'picsum.photos',
             },
