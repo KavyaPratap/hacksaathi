358c7e20b08bb6b34994743829600d49d8d60ea6
KavyaPratap Singh <kavyapratapsingh221@gmail.com>
2025-08-27 09:33:29 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,37 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            /** @type {import('next').NextConfig} */
-
-              experimental: {
-                serverActions: {
-                  bodySizeLimit: '10mb',
-                },
-              },
-              images: {
-                remotePatterns: [
-                  {
-                    protocol: 'https',
-                    hostname: 'uzippzfxkhteeajhcppp.supabase.co',
-                    port: '',
-                    pathname: '/storage/v1/object/public/**',
-                  },
-            
-            {    
-                protocol: 'https',
-                hostname: 'picsum.photos',
-            },
-            {
-                protocol: 'https',
-                hostname: 'lh3.googleusercontent.com',
-            },
-            {
-                protocol: 'https',
-                hostname: 'uzippzfxkhteeajhcppp.supabase.co',
-            }
-        ]
-    }
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '10mb',
+    },
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'uzippzfxkhteeajhcppp.supabase.co',
+        port: '',
+        pathname: '/storage/v1/object/public/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
